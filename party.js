091771f27bf6ab1d301c44c910d823d3c2ede4ff
Doug Leary <DougLeary@gmail.com>
2024-04-03 03:06:10 -0700

    fix multiclass xpToNext, add henchmen and hiding

--- party.js
+++ party.js
@@ -22,20 +22,52 @@ function getPartyLevels(party, xp) {
   const result = {name: party.name, xp: (xp == 0) ? party.xp : xp, members: []}
   for (let m in party.members) {
     const mem = party.members[m]
+    if (mem.hasOwnProperty("hide")) { continue }   // don't show hidden members
     const member = {name: mem.name, class: mem.class, level: "0"}
     const xpParam = (xp > 0) ? xp : party.xp
-    const useXp = (mem.xpBonus) ? Math.floor(xpParam * 1.1) : xpParam
     const chClasses = mem.class.split('/')
-    let chLevel = ''
-    let xpToNext = ''
+    const chLevel = []
+    let xpToNext = []
     for (let c in chClasses) {
-      const lev = levels.getCharacterLevel(mem.edition, chClasses[c], Math.floor(useXp / chClasses.length), mem.xpBonus)
-      chLevel += lev.level + '/'
-      xpToNext += lev.xpToNext + '/'
+      const getsBonus = (mem.xpBonus.substring(c,c+1) == 'y')
+      const useXp = getsBonus ? Math.floor(xpParam * 1.1) : xpParam
+      //console.log(`${member.name} bonus: ${mem.xpBonus}, ${c}:${mem.xpBonus.substring(c,c)} ${chClasses[c]} useXp ${useXp} getsBonus ${getsBonus}`)
+      const lev = levels.getCharacterLevel(mem.edition, chClasses[c], Math.floor(useXp / chClasses.length))
+      chLevel.push(lev.level)
+      xpToNext .push(getsBonus ? Math.floor(lev.xpToNext / 1.1) : lev.xpToNext)
     }
-    member.level = chLevel.slice(0,-1)
-    member.xpToNext = xpToNext.slice(0,-1)
+    if (mem.hasOwnProperty("boss")) {
+      member.boss = mem.boss
+      member.level = ''
+      member.xpToNext = ''
+    } else {
+      member.level = chLevel.join('/')
+      member.xpToNext = xpToNext.join('/')
+    }
     result.members.push(member)
+  }
+  // assign all henchman levels to boss's lowest level - 2
+  for (let m in result.members) {
+    const hench = result.members[m]
+    if (hench.hasOwnProperty("boss")) {
+      let minBossLevel = 99999
+      // find the boss
+      for (let b in result.members) {
+        if (result.members[b].name == hench.boss) {
+          // get the boss's lowest level
+          const bossLevels = result.members[b].level.split('/')
+          minBossLevel = bossLevels[0]
+          for (let i = 1; i < bossLevels.length; i++) {
+            minBossLevel = Math.min(minBossLevel, bossLevels[i])
+          }
+        }
+      }
+      const henchLevels = []
+      for (let c in hench.class.split('/')) {
+        henchLevels.push(minBossLevel - 2)
+      }
+      hench.level = henchLevels.join('/')
+    }
   }
   return result
 }
